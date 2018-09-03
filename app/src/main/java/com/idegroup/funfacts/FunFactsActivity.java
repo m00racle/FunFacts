d1e1bf79ec6660dba2f4dd9d99ed7462f2afc967
mooracle <yanuar.prakosa@gmail.com>
2018-09-03 08:49:05 +0700

    Add relative layout color changer statically

--- app/src/main/java/com/idegroup/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/idegroup/funfacts/FunFactsActivity.java
@@ -1,12 +1,12 @@
 package com.idegroup.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import java.util.Random;
 
 /*
 * In Android Activity represent the screen that user can interact with
@@ -18,6 +18,10 @@ public class FunFactsActivity extends AppCompatActivity {
     * this session it omit that means it follows the general programming format..*/
     private TextView factTextView;
     private Button showFactButton;
+
+    /*in order to change the background color dynamically we need to define the background of the activity:*/
+
+    private RelativeLayout relativeLayout;/*<- this is where the background of this activity is defined*/
 
     /*insert newly random fact by instantiating FactBook object to this activity:*/
 
@@ -40,6 +44,8 @@ public class FunFactsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fun_facts);/*<- R here means generated class by Android called Resources!!
         it's located: /home/idegroup/IdeProject/FunFacts/app/build/generated/source/r/debug/com/idegroup/funfacts/R.java*/
 
+
+
         /*Assign the views from the Layout File to corresponding variable:
         * NOTE: assign views only after the setContentView!!!
         *
@@ -48,12 +54,18 @@ public class FunFactsActivity extends AppCompatActivity {
         factTextView = findViewById(R.id.factTextView);/*<- this R can find the int id which requested by findViewByID*/
         showFactButton = findViewById(R.id.showFactButton);/*<- pelase remember we set the id of our button as this!*/
 
+        relativeLayout = findViewById(R.id.factRelativeLayout);/*<- refer to factActivityPage for activity group view*/
+
         /*Now we will make our button do something:*/
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 factTextView.setText(factBook.getFact());/*<- getFact from the factBook object to be shown*/
+
+                /*Now changing to new color when we press the button:*/
+
+                relativeLayout.setBackgroundColor(Color.RED);/*<- more of the usage of Color class in the Documentation*/
             }
         };
         showFactButton.setOnClickListener(listener);
